53b12d64e531ab644b4eb9b22f0b6ba96e6411b0
Paulo Fortes <pauloffn@gmail.com>
2018-04-26 15:30:30 -0300

    listagem apenas com botões

--- src/components/listagem_dummy.tsx
+++ src/components/listagem_dummy.tsx
@@ -9,22 +9,22 @@ interface Props {
     clicado: boolean;
 }
 
-interface HProps {
-    children: string;    
-}
+// interface HProps {
+//     children: string;    
+// }
 
-const Hx: React.SFC<HProps> = (props) => {
-    return (
-        <h2 
-            style={{
-                display: 'inline-block',
-                color: '#ecc',
-            }}
-        >
-        {' '}{props.children}{' '}
-        </h2>
-    );
-};
+// const Hx: React.SFC<HProps> = (props) => {
+//     return (
+//         <h2 
+//             style={{
+//                 display: 'inline-block',
+//                 color: '#ecc',
+//             }}
+//         >
+//         {' '}{props.children}{' '}
+//         </h2>
+//     );
+// };
 
 const Listagem: React.SFC<Props> = (props) => {
     const { dst, copy, situacao, separador, botaoAtivo, clicado } = props;
@@ -32,22 +32,19 @@ const Listagem: React.SFC<Props> = (props) => {
         (numProc) => situacao[numProc] === dst
     );
     return (
-        <div>
-            {botaoAtivo && <Hx> * </Hx>}
-            {clicado && <Hx> [OK] </Hx>}
-            <h2 style={{ display: 'inline-block' }}>{dst}</h2>
-            {botaoAtivo && <Hx> * </Hx>}
+        <div className="listagem-div">            
         <button
                 style={{ 
                     display: 'inline-block',
-                    backgroundColor: botaoAtivo ? '#cce' : 'unset',
+                    backgroundColor: botaoAtivo ? '#cce' : clicado ? '#eee' : 'none',
+                    color: clicado ? '#ccc' : 'black',
+
                 }}
                 onClick={() => copy(arr.join(separador), dst)}
-        > copia {dst}
-        </button>        
-            <div style={{ textOverflow: '' }}>{
-                arr.map(a => <span style={{ display: 'inline-block', margin: 3 }} key={'k' + a}>{a}</span>)}</div>
+        > COPIAR {dst} ({arr.length})
+        </button>
         </div>
+            
     );
 
 };
