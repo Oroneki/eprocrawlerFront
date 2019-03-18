4f1bda2690a3de73a3227ce9230342eaa0ba3278
Paulo Fortes <pauloffn@gmail.com>
2019-03-18 12:38:44 -0300

    hooks ok

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,17 +2,22 @@
 exports.__esModule = true;
 var fs = require("fs-extra");
 var path = require("path");
-console.log(process.argv[2]);
-fs.copy('./build', process.argv[2])
-    .then(function () { return console.log("Copiado ./build para " + process.argv[2]); })
+var destPath = path.resolve(__dirname, process.argv[2]);
+if (!fs.existsSync(destPath)) {
+    console.info(">", destPath, "não existe.");
+    process.exit(1);
+}
+console.log(process.argv[2], "-->", destPath);
+fs.copy("./build", destPath)
+    .then(function () { return console.log("Copiado ./build para " + destPath); })
     .then(injectIndex)["catch"](function (err) { return console.log(err); });
 function injectIndex() {
-    console.log('');
-    var str = fs.readFileSync('./build/index.html').toString();
+    console.log("");
+    var str = fs.readFileSync("./build/index.html").toString();
     var regex = /<script>\s?window\.epro.*?<\/script>/gm;
     console.log(regex);
     str = str.replace(regex, "<script>window.eprocData={{.Data}};window.PORT_SERVER={{.Port}}</script>");
     console.log(str);
-    fs.writeFileSync(path.join(process.argv[2], 'index.html'), str);
-    console.log('Salvo!');
+    fs.writeFileSync(path.join(process.argv[2], "index.html"), str);
+    console.log("Salvo!");
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,29 @@
-import * as fs from 'fs-extra';
-import * as path from 'path';
+import * as fs from "fs-extra";
+import * as path from "path";
 
-console.log(process.argv[2]);
-fs.copy('./build', process.argv[2])
-    .then(
-        () => console.log(`Copiado ./build para ${process.argv[2]}`)
-    )
-    .then(injectIndex)
-    .catch(
-        err => console.log(err)
-    );
+const destPath = path.resolve(__dirname, process.argv[2]);
+
+if (!fs.existsSync(destPath)) {
+  console.info(">", destPath, "não existe.");
+  process.exit(1);
+}
+
+console.log(process.argv[2], "-->", destPath);
+fs.copy("./build", destPath)
+  .then(() => console.log(`Copiado ./build para ${destPath}`))
+  .then(injectIndex)
+  .catch(err => console.log(err));
 
 function injectIndex() {
-    console.log('');
-    let str = fs.readFileSync('./build/index.html').toString();
-    const regex = /<script>\s?window\.epro.*?<\/script>/gm;
-    console.log(regex);
-    str = str.replace(regex, `<script>window.eprocData={{.Data}};window.PORT_SERVER={{.Port}}</script>`);
-    console.log(str);
-    fs.writeFileSync(path.join(process.argv[2], 'index.html'), str);
-    console.log('Salvo!');
+  console.log("");
+  let str = fs.readFileSync("./build/index.html").toString();
+  const regex = /<script>\s?window\.epro.*?<\/script>/gm;
+  console.log(regex);
+  str = str.replace(
+    regex,
+    `<script>window.eprocData={{.Data}};window.PORT_SERVER={{.Port}}</script>`
+  );
+  console.log(str);
+  fs.writeFileSync(path.join(process.argv[2], "index.html"), str);
+  console.log("Salvo!");
 }
